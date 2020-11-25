9b45983e9f5a018b9aa72492849ec69bbc932779
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-11-26 00:52:17 +0300

    modified models

--- employer/models.py
+++ employer/models.py
@@ -4,12 +4,10 @@ from django.db import models
 # Create your models here.
 
 class Detail(models.Model):
-    field = models.TextField(max_length=50)
-    subfield = models.TextField(max_length=100)
-    company_name = models.TextField(max_length=50)
-    county = models.TextField(max_length=50)
-    email = models.CharField(max_length=25)
-    phone = models.CharField(max_length=15)
-    salary = models.IntegerField()
-    years = models.FloatField()
+    job_title = models.TextField()
+    company = models.TextField()
+    county = models.CharField()
+    email = models.EmailField()
+    phone_number = models.TextField()
+    position = models.BooleanField()
 
