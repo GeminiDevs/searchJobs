2ee9a73b9d678cd3d5d5c6a6f3d07f476a69e67e
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-11-26 00:55:59 +0300

    modified models

--- employer/models.py
+++ employer/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Detail(models.Model):
     job_title = models.TextField()
     company = models.TextField()
-    county = models.CharField()
+    county = models.CharField(max_length=15)
     email = models.EmailField()
     phone_number = models.TextField()
     position = models.BooleanField()
