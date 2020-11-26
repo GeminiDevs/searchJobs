8a5b85bf67ee04381328fee51989a4857662153e
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-11-26 11:03:15 +0300

    modified models

--- employer/models.py
+++ employer/models.py
@@ -4,10 +4,12 @@ from django.db import models
 # Create your models here.
 
 class Detail(models.Model):
+    internship = True
+    fulltime = False
     job_title = models.TextField()
     company = models.TextField()
     county = models.CharField(max_length=15)
     email = models.EmailField()
     phone_number = models.TextField()
-    position = models.BooleanField()
+    position = models.BooleanField(default=internship)
 
